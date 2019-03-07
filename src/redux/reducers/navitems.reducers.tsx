eab9b06ee678ace00bc7b02fd4ae7a952a2e18fb
Arnab <grinit10@gmail.com>
2019-03-08 03:17:25 +1100

    Converting navitems to array

--- src/redux/reducers/navitems.reducers.tsx
+++ src/redux/reducers/navitems.reducers.tsx
@@ -1,9 +1,9 @@
 import { navsActions } from '../navitems.actions';
 import {NavItem} from '../../interfaces/NavItem';
 
-const initState = {}
+const initState : Array<NavItem> = []
 
-export const navsReducer = (state = initState, action: {type: string, payload: NavItem}) => {
+export const navsReducer = (state = initState, action: {type: string, payload: Array<NavItem>}) => {
     switch (action.type) {
         case navsActions.LOAD_NAVS_DONE:
             return { ...state, ...action.payload }
